e44631bf993ed3bbfb0bcf21310fb5993ce07dd4
Iñaki Villar <inaki.seri@gmail.com>
2026-07-24 19:17:38 -0700

    Onboard to the OSS Community Develocity Instance (#182)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,36 @@
 plugins {
+    id("com.gradle.develocity") version "4.4.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.6.0"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+
+val isCI = System.getenv("CI") != null
+
+develocity {
+    server = "https://community.develocity.cloud"
+    projectId = "rnett"
+    buildScan {
+        uploadInBackground = !isCI
+        publishing.onlyIf { it.isAuthenticated }
+        obfuscation {
+            ipAddresses { addresses -> addresses.map { _ -> "0.0.0.0" } }
+        }
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = true
+    }
+
+    remote(develocity.buildCache) {
+        isEnabled = true
+        // Check access key presence to avoid build cache errors on PR builds when access key is not present
+        val accessKey = System.getenv("DEVELOCITY_ACCESS_KEY")
+        isPush = isCI && accessKey != null
+    }
+}
+
 rootProject.name = "gradle-mcp"
 include("repl-worker", "repl-shared")
 
